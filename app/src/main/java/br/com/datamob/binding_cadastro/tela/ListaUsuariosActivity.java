d825e168fdb46ec16fbf5bee2d20a53fcb3c91c4
Felippe Neves <felippe@datamob.com.br>
2019-11-26 08:38:19 -0300

    OK

--- app/src/main/java/br/com/datamob/binding_cadastro/tela/ListaUsuariosActivity.java
+++ app/src/main/java/br/com/datamob/binding_cadastro/tela/ListaUsuariosActivity.java
@@ -24,7 +24,6 @@ public class ListaUsuariosActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_usuarios);
         inicializaComponentes();
         carregaInformacoes();
     }
